9f34acc9ffce79cd9242c6f45a44c76a709c0e4e
Rafael Makaha <rafael.makaha@gmail.com>
2020-04-12 16:38:58 -0300

    Adds basic bot structure.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/index.py
+++ src/index.py
@@ -1,9 +1,32 @@
 import discord
+from discord.ext import commands
+import datetime
 
-from discord.txt import commands
-
-commands.Bot(command_prefix=">", description="This is a helper bot!")
+bot = commands.Bot(command_prefix=">", description="This is a helper bot!")
 
 @bot.command()
-def ping(ctx):
-    ctx.send("pong")
+async def ping(ctx):
+    await ctx.send("pong")
+
+@bot.command()
+async def sum(ctx, n1: int, n2: int):
+    await ctx.send(n1 + n2)
+
+@bot.command()
+async def status(ctx):
+    embed = discord.Embed(title=f'{ctx.guild.name}', 
+    description="Alguma descrição", 
+    timestamp=datetime.datetime.utcnow(), 
+    color=discord.Color.blue())
+    embed.add_field(name="Debug", value=discord.ClientUser.avatar)
+    await ctx.send(embed=embed)
+
+
+    
+
+# Events
+@bot.event
+async def on_ready():
+    print("The bot is ready!")
+
+bot.run('')
