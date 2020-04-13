23d9af3c42001f6355ac517315a5eb92a9ddc34d
Rafael Makaha <rafael.makaha@gmail.com>
2020-04-13 16:42:51 -0300

    Changes message presentation.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/index.py
+++ src/index.py
@@ -54,7 +54,9 @@ async def patentes(ctx):
     for item in list(options.items()):
         content.append(str(str(item[0] + '  :  ' + str(item[1]))))
     
-    embed = discord.Embed(title='Patentes', description=('\n'.join(content)))
+    embed = discord.Embed(title='Patentes', 
+    description=('\n'.join(content)),
+    color=discord.Color.magenta())
     await ctx.send(embed=embed)
 
 @bot.command()
@@ -62,17 +64,25 @@ async def add(ctx, name, weight: int):
     server = ctx.guild
 
     if weight < 0 or weight > 18:
-        return await ctx.send("Valor fora dos pesos das patentes.")
+        embed = discord.Embed(title="MixCS", 
+        description="Valor fora dos pesos das patentes.",
+        color=discord.Color.magenta())
+        return await ctx.send(embed=embed)
 
     if server.id in players:
         if len(players[server.id]) < 10:
             players[server.id].append((name,weight))
         else:
-            return await ctx.send("Não há espaço para mais jogadores.")
+            embed = discord.Embed(title="MixCS", 
+            description="Não há espaço para mais jogadores.",
+            color=discord.Color.magenta())
+            return await ctx.send(embed=embed)
     else:
         players[server.id] = [(name,weight)]
-    
-    await ctx.send(name + " adicionado.")
+    embed = discord.Embed(title="MixCS", 
+    description=f'{name} adicionado.',
+    color=discord.Color.magenta())
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def queue(ctx):
@@ -83,9 +93,15 @@ async def queue(ctx):
         for index,name in enumerate(players[server.id]):
             response.append("{}. {}".format(index+1,name[0]))
 
-        await ctx.send('\n'.join(response))
+        embed = discord.Embed(title="MixCS", 
+        description=('\n'.join(response)),
+        color=discord.Color.magenta())
+        return await ctx.send(embed=embed)
     else:
-        await ctx.send("Não há nenhum jogador na fila.")
+        embed = discord.Embed(title="MixCS", 
+        description="Não há nenhum jogador na fila.",
+        color=discord.Color.magenta())
+        return await ctx.send(embed=embed)
 
 @bot.command()
 async def remove(ctx, pos: int):
@@ -95,11 +111,20 @@ async def remove(ctx, pos: int):
     if server.id in players:
         if len(players[server.id]) > int(pos):
             name = players[server.id].pop(int(pos))
-            await ctx.send('{} removido da fila.'.format(name[0]))
+            embed = discord.Embed(title="MixCS", 
+            description="{} removido da fila.".format(name[0]),
+            color=discord.Color.magenta())
+            return await ctx.send(embed=embed)
         else:
-            await ctx.send("Esta posição não exite na fila.")
+            embed = discord.Embed(title="MixCS", 
+            description="Esta posição não exite na fila.",
+            color=discord.Color.magenta())
+            return await ctx.send(embed=embed)
     else:
-        await ctx.send("Não há jogadores na fila.")
+        embed = discord.Embed(title="MixCS", 
+        description="Não há jogadores na fila.",
+        color=discord.Color.magenta())
+        return await ctx.send(embed=embed)
 
 @bot.command()
 async def clear(ctx):
@@ -107,9 +132,15 @@ async def clear(ctx):
 
     if server.id in players:
         players[server.id] = []
-        await ctx.send("Todos os jogadores foram removidos.")
+        embed = discord.Embed(title="MixCS", 
+        description="Todos os jogadores foram removidos.",
+        color=discord.Color.magenta())
+        return await ctx.send(embed=embed)
     else:
-        await ctx.send("Não há jogadores na fila.")
+        embed = discord.Embed(title="MixCS", 
+        description="Não há jogadores na fila.",
+        color=discord.Color.magenta())
+        return await ctx.send(embed=embed)
 
 @bot.command()
 async def run(ctx):
@@ -133,11 +164,26 @@ async def run(ctx):
             t2.append(tier2.pop(tier2.index(random.choice(tier2))))
             t1.append(tier3.pop(tier3.index(random.choice(tier3))))
             t2.append(tier3.pop(tier3.index(random.choice(tier3))))
-
-        await ctx.send(t1)
-        await ctx.send(t2)
+        
+        response1 = []
+        response2 = []
+        for index in range(5):
+            response1.append("{}. {}".format(index+1,t1[index][0]))
+            response2.append("{}. {}".format(index+1,t2[index][0]))
+        
+        embed = discord.Embed(title="MixCS", 
+        description="Times sorteados",
+        color=discord.Color.magenta())
+        embed.add_field(name="Time A", value="\n".join(response1))
+        embed.add_field(name="Time B", value="\n".join(response2))
+        return await ctx.send(embed=embed)
+        # await ctx.send(t1)
+        # await ctx.send(t2)
     else:
-        await ctx.send("Não há jogadores suficientes para formar os times.")
+        embed = discord.Embed(title="MixCS", 
+        description="Não há jogadores suficientes para formar os times.",
+        color=discord.Color.magenta())
+        return await ctx.send(embed=embed)
 
 # Events
 @bot.event
