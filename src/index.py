d5d4667f83dc688099f0f2229bc6eb5a1a225532
Rafael Makaha <rafael.makaha@gmail.com>
2020-04-13 17:12:14 -0300

    Removes status method.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/index.py
+++ src/index.py
@@ -30,22 +30,14 @@ options = {
 
 players = {}
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
-@bot.command()
-async def sum(ctx, n1: int, n2: int):
-    await ctx.send(n1 + n2)
-
-@bot.command()
-async def status(ctx):
-    embed = discord.Embed(title=f'{ctx.guild.name}', 
-    description="Alguma descrição", 
-    timestamp=datetime.datetime.utcnow(), 
-    color=discord.Color.blue())
-    embed.add_field(name="Debug", value=discord.ClientUser.avatar)
-    await ctx.send(embed=embed)
+# @bot.command()
+# async def status(ctx):
+#     embed = discord.Embed(title=f'{ctx.guild.name}', 
+#     description="Alguma descrição", 
+#     timestamp=datetime.datetime.utcnow(), 
+#     color=discord.Color.blue())
+#     embed.add_field(name="Debug", value=discord.ClientUser.avatar)
+#     await ctx.send(embed=embed)
 
 @bot.command()
 async def patentes(ctx):
